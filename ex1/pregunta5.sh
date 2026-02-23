4d2a42870a5fd8368faf915469d5a7deae3562f5
fijisci <joseluisgrr@yahoo.com>
2026-02-23 17:37:14 -0600

    pregunta5

---
+++ ex1/pregunta5.sh
@@ -0,0 +1,109 @@
+  1  sudo apt update
+    2  sudo apt upgrade
+    3  vim
+    4  sudo apt install vim
+    5  vim
+    6  clear
+    7  ls
+    8  sudo apt install cmake
+    9  python
+   10  sudo apt install vscode
+   11  cd Downloads/
+   12  ls
+   13  sudo apt install code_1.108.1-1768404234_amd64.deb
+   14  sudo apt install ./code_1.108.1-1768404234_amd64.deb 
+   15  cd Desktop
+   16  mkdir SO
+   17  cd SO
+   18  top
+   19  dir
+   20  cd Desktop
+   21  dir
+   22  del SO
+   23  rm SO
+   24  rmdir SO
+   25  exit
+   26  cd ~
+   27  open .bashrc
+   28  cd SO/ex1
+   29  code meme.txt
+   30  dir
+   31  cd SO
+   32  ls
+   33  mkdir ex1
+   34  cd ex1
+   35  touch meme.txt
+   36  dir
+   37  code meme.txt
+   38  grep "Python"  meme.txt
+   39  man cron
+   40  ls | grep "Python" meme.txt
+   41  ls
+   42  grep cron
+   43  history | grep grep
+   44  ls
+   45  cd ../
+   46  dir
+   47  ls | grep .cpp
+   48  man peek
+   49  peek
+   50  ls
+   51  cd ex1
+   52  ls
+   53  cd .//
+   54  cd ../
+   55  cd ex1; ls | grep meme
+   56  echo holi
+   57  echo "alias"
+   58  echo "alias code=codium"
+   59  echo "alias code=codium" >> .bashrc
+   60  dir
+   61  history
+   62  dir
+   63  echo "holiwis" >> .txt
+   64  vim meme.txt
+   65  cd .../
+   66  cd ..../
+   67  cd ../
+   68  dir
+   69  open 
+   70  man open
+   71  open .bashrc
+   72  man echo
+   73  open .bashrc
+   74  .bashrx | grep code
+   75  .bashrc | grep code
+   76  cat .bashrc | grep code
+   77  cat .bashrc | grep alias
+   78  ll
+   79  cd SO
+   80  dir
+   81  cd ex1
+   82  ls
+   83  codium meme.txt
+   84  pwr
+   85  pwn
+   86  pwd
+   87  cd ~
+   88  echo "alias code=codium" >> .bashrc
+   89  cd /SO/ex1; code meme.txt
+   90  cd SO/ex1; code meme.txt
+   91  echo $PATH
+   92  man export
+   93  whaaat
+   94  export
+   95  echo $PATH
+   96  lista
+   97  echo lista
+   98  dir
+   99  echo ls > meme.txt
+  100  cat meme.txt
+  101  ls > meme.txt
+  102  cat meme.txt
+  103  history | grep cd
+  104  history | grep cd >> accept.txt
+  105  cat accept.txt
+  106  rm accept.txt
+  107  touch accept.txt; ls > accept.txt; history | grep cd >> accept.txt
+  108  cat accept.txt
+  109  history
