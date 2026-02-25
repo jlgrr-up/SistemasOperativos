cb8ed6930ef29541dbd27ea2462a1b9f02d01113
fijisci <joseluisgrr@yahoo.com>
2026-02-25 16:41:55 -0600

    eliminando programas innecesarios

--- primera_entrega/j.cpp
+++
@@ -1,8 +0,0 @@
-#include <chrono>
-#include <iostream>
-
-int main(){
-    std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
-    std::cout << now.time_since_epoch().count() << std::endl;
-    return 0;
-}
